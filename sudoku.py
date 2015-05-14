542ffab77c03f6c024e21f17bbc1d690be6b3e7f
AiryShift <religionofaperturelabs@gmail.com>
2015-05-14 10:45:40 +1000

    Add further console logging, fix time output
    Too much C leads to printf() syntax

--- sudoku.py
+++ sudoku.py
@@ -3,15 +3,18 @@ from time import time
 
 now = time()
 print('Opening the file...')
+print('Found this sudoku inside...\n')
 with open('sudoin.txt') as f:
     board = []
     for i in f:
+        print(i.strip())
         board.append([int(num) for num in i.strip()])
 
-print('Processing the board...')
+print('\nSolving the sudoku...\n')
 with open('sudoout.txt', 'w') as f:
     for i in SudokuBoard(board).solve():
+        print(''.join([str(num) for num in i]))
         print(''.join([str(num) for num in i]), file=f)
 
-print('Done!')
-print('Took {} seconds long.', time() - now)
+print('\nDone!')
+print('Took {} seconds long.'.format(time() - now))
